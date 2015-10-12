453a8effe2beb24a837e4185b2fa42cf44f1bec4
Larry Nickerson <lnickers@austin.rr.com>
2015-10-12 17:48:21 -0500

    updated BMSClient to use guard to test for invalid route and guid.

--- src/ios/CDVBMSClient.swift
+++ src/ios/CDVBMSClient.swift
@@ -11,35 +11,59 @@ import IMFCore
 @objc(CDVBMSClient) class CDVBMSClient : CDVPlugin {
     
     func initialize(command: CDVInvokedUrlCommand) {
-        let route = command.arguments[0] as! String
-        let guid = command.arguments[1] as! String
-        let client = IMFClient.sharedInstance()
         
-        //use category to handle objective-c exception
-        let exceptionString = client.tryInitializeWithBackendRoute(route, backendGUID: guid)
-        
-        if exceptionString  == "" {
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "")
-            commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        guard let route  = command.arguments[0] as? String else {
+            let message = "Invalid route."
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: message)
+            // call success callback
+            self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            return
         }
-        else{
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: exceptionString)
-            commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        
+        guard let guid = command.arguments[1] as? String else{
+            let message = "Invalid guid."
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: message)
+            // call failure callback
+            self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            return
+        }
+        
+        self.commandDelegate!.runInBackground({
+            let client = IMFClient.sharedInstance()
             
-        }
+            //use category to handle objective-c exception
+            let exceptionString = client.tryInitializeWithBackendRoute(route, backendGUID: guid)
+            
+            if exceptionString  == "" {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "")
+                // call success callback
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            }
+            else{
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: exceptionString)
+                // call failure callback
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            }
+        })
     }
     
     func backendRoute(command: CDVInvokedUrlCommand) {
+        
         let client = IMFClient.sharedInstance()
         let backendRoute: String = client.backendRoute
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: backendRoute)
-        commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        // call success callback
+        self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        
     }
     
     func backendGUID(command: CDVInvokedUrlCommand) {
+        
         let client = IMFClient.sharedInstance()
         let backendGUID: String = client.backendGUID
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: backendGUID)
-        commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        // call success callback
+        self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        
     }
 }
