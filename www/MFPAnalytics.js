1571640a5a2a98a91ddd11aa4f040094980d65a2
Sergio Escoto <escotos@us.ibm.com>
2015-10-23 11:28:47 -0500

    Moved default callbacks to outer scope

--- www/MFPAnalytics.js
+++ www/MFPAnalytics.js
@@ -12,9 +12,11 @@
 */
 var exec = require("cordova/exec");
 
+var success = function() { console.log("Success: MFPAnalytics "); };
+var failure = function() { console.log("Error: MFPAnalytics"); };
+
 var MFPAnalytics = {
-    success: function() { console.log("Success: MFPAnalytics "); },
-    failure: function() { console.log("Error: MFPAnalytics"); },
+
     /**
      * Turns on the global setting for persisting of the analytics data.
      */
