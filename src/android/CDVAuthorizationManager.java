ba97325c0e2756917b826366de18e48cf5d28d19
rotembr <rotembr@il.ibm.com>
2015-10-27 18:22:46 +0200

    Running all fun in different thread for improving UI performance.

--- src/android/CDVAuthorizationManager.java
+++ src/android/CDVAuthorizationManager.java
@@ -45,39 +45,31 @@ public class CDVAuthorizationManager extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if("obtainAuthorizationHeader".equals(action)) {
+        if ("obtainAuthorizationHeader".equals(action)) {
             this.obtainAuthorizationHeader(callbackContext);
             return true;
-        }
-        else if("clearAuthorizationData".equals(action)) {
-            this.clearAuthorizationData();
+        } else if ("clearAuthorizationData".equals(action)) {
+            this.clearAuthorizationData(callbackContext);
             return true;
-        }
-        else if("isAuthorizationRequired".equals(action)) {
+        } else if ("isAuthorizationRequired".equals(action)) {
             this.isAuthorizationRequired(args, callbackContext);
             return true;
-        }
-        else if("getCachedAuthorizationHeader".equals(action)) {
+        } else if ("getCachedAuthorizationHeader".equals(action)) {
             this.getCachedAuthorizationHeader(callbackContext);
             return true;
-        }
-        else if("getAuthorizationPersistencePolicy".equals(action)) {
+        } else if ("getAuthorizationPersistencePolicy".equals(action)) {
             this.getAuthorizationPersistencePolicy(callbackContext);
             return true;
-        }
-        else if("setAuthorizationPersistencePolicy".equals(action)) {
-            this.setAuthorizationPersistencePolicy(args);
+        } else if ("setAuthorizationPersistencePolicy".equals(action)) {
+            this.setAuthorizationPersistencePolicy(args, callbackContext);
             return true;
-        }
-        else if("getUserIdentity".equals(action)) {
+        } else if ("getUserIdentity".equals(action)) {
             this.getUserIdentity(callbackContext);
             return true;
-        }
-        else if("getAppIdentity".equals(action)) {
+        } else if ("getAppIdentity".equals(action)) {
             this.getAppIdentity(callbackContext);
             return true;
-        }
-        else if("getDeviceIdentity".equals(action)) {
+        } else if ("getDeviceIdentity".equals(action)) {
             this.getDeviceIdentity(callbackContext);
             return true;
         }
@@ -122,70 +114,109 @@ public class CDVAuthorizationManager extends CordovaPlugin {
 
     }
 
-    private void clearAuthorizationData() {
-        AuthorizationManager.getInstance().clearAuthorizationData();
-        AMLogger.debug("Authorization data cleared.");
+    private void clearAuthorizationData(final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                AuthorizationManager.getInstance().clearAuthorizationData();
+                AMLogger.debug("Authorization data cleared.");
+                callbackContext.success();
+            }
+        });
     }
 
-    private void isAuthorizationRequired(JSONArray args, CallbackContext callbackContext) throws JSONException {
+    private void isAuthorizationRequired(final JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
-        try {
-            int statusCode = args.getInt(0);
-            String responseAuthorizationHeader = args.getString(1);
-            boolean answer = AuthorizationManager.getInstance().isAuthorizationRequired(statusCode, responseAuthorizationHeader);
-            AMLogger.debug("isAuthorizationRequired return "+ answer);
-            callbackContext.success(String.valueOf(answer));
-        } catch (JSONException e) {
-            callbackContext.error(e.getMessage());
-        }
-
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try {
+                    int statusCode = args.getInt(0);
+                    String responseAuthorizationHeader = args.getString(1);
+                    boolean answer = AuthorizationManager.getInstance().isAuthorizationRequired(statusCode, responseAuthorizationHeader);
+                    AMLogger.debug("isAuthorizationRequired return " + answer);
+                    callbackContext.success(String.valueOf(answer));
+                } catch (JSONException e) {
+                    callbackContext.error(e.getMessage());
+                }
+            }
+        });
     }
 
-    private void getCachedAuthorizationHeader(CallbackContext callbackContext) {
-        String header = AuthorizationManager.getInstance().getCachedAuthorizationHeader();
-        AMLogger.debug("Cached authorization header: " + header);
-        callbackContext.success(header);
+
+    private void getCachedAuthorizationHeader(final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                String header = AuthorizationManager.getInstance().getCachedAuthorizationHeader();
+                AMLogger.debug("Cached authorization header: " + header);
+                callbackContext.success(header);
+            }
+        });
     }
 
-    private void getAuthorizationPersistencePolicy(CallbackContext callbackContext) {
-        AuthorizationManager.PersistencePolicy policy =  AuthorizationManager.getInstance().getAuthorizationPersistencePolicy();
-        AMLogger.debug("PersistencePolicy:" + policy.toString());
-        callbackContext.success(policy.toString());
+    private void getAuthorizationPersistencePolicy(final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                AuthorizationManager.PersistencePolicy policy = AuthorizationManager.getInstance().getAuthorizationPersistencePolicy();
+                AMLogger.debug("PersistencePolicy:" + policy.toString());
+                callbackContext.success(policy.toString());
+            }
+        });
     }
 
-    private void setAuthorizationPersistencePolicy(JSONArray args) throws JSONException{
-        String policy = args.getString(0);
+    private void setAuthorizationPersistencePolicy(final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                String newPolicy = null;
+                try {
+                    newPolicy = args.getString(0).toUpperCase();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                boolean success = true;
+                if (newPolicy.equals(PersistencePolicyNever)) {
+                    AuthorizationManager.getInstance().setAuthorizationPersistencePolicy(AuthorizationManager.PersistencePolicy.NEVER);
+                } else if (newPolicy.equals(PersistencePolicyAlways)) {
+                    AuthorizationManager.getInstance().setAuthorizationPersistencePolicy(AuthorizationManager.PersistencePolicy.ALWAYS);
+                } else {
+                    success = false;
+                    AMLogger.debug("Policy cann't be recognized:" + newPolicy.toString());
+                    callbackContext.error("The specified persistence policy is not supported.");
+                }
 
-        if (policy.equals(PersistencePolicyAlways)) {
-            AuthorizationManager.getInstance().setAuthorizationPersistencePolicy(AuthorizationManager.PersistencePolicy.ALWAYS);
-            AMLogger.debug("PersistencePolicy set to:" + policy.toString());
-
-        }
-        else if(policy.equals(PersistencePolicyNever)) {
-            AuthorizationManager.getInstance().setAuthorizationPersistencePolicy(AuthorizationManager.PersistencePolicy.NEVER);
-            AMLogger.debug("PersistencePolicy set to:" + policy.toString());
-
-        }else {
-            //should not get to this code, hybrid code handle the error
-            AMLogger.debug("Policy cann't be recognized:" + policy.toString());
-        }
+                if (success) {
+                    AMLogger.debug("PersistencePolicy set to:" + newPolicy.toString());
+                    callbackContext.success();
+                }
+            }
+        });
     }
 
-    private void getUserIdentity(CallbackContext callbackContext) {
-        UserIdentity userIdentity =  AuthorizationManager.getInstance().getUserIdentity();
-        AMLogger.debug("userIdentity: " + userIdentity.toString());
-        callbackContext.success(userIdentity.toString());
+    private void getUserIdentity(final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                UserIdentity userIdentity = AuthorizationManager.getInstance().getUserIdentity();
+                AMLogger.debug("userIdentity: " + userIdentity.toString());
+                callbackContext.success(userIdentity.toString());
+            }
+        });
     }
 
-    private void getAppIdentity(CallbackContext callbackContext) {
-        AppIdentity appIdentity =  AuthorizationManager.getInstance().getAppIdentity();
-        AMLogger.debug("appIdentity: " + appIdentity.toString());
-        callbackContext.success(appIdentity.toString());
+    private void getAppIdentity(final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                AppIdentity appIdentity = AuthorizationManager.getInstance().getAppIdentity();
+                AMLogger.debug("appIdentity: " + appIdentity.toString());
+                callbackContext.success(appIdentity.toString());
+            }
+        });
     }
 
-    private void getDeviceIdentity(CallbackContext callbackContext) {
-        DeviceIdentity deviceIdentity = AuthorizationManager.getInstance().getDeviceIdentity();
-        AMLogger.debug("deviceIdentity: " + deviceIdentity.toString());
-        callbackContext.success(deviceIdentity.toString());
+    private void getDeviceIdentity(final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                DeviceIdentity deviceIdentity = AuthorizationManager.getInstance().getDeviceIdentity();
+                AMLogger.debug("deviceIdentity: " + deviceIdentity.toString());
+                callbackContext.success(deviceIdentity.toString());
+            }
+        });
     }
 }
