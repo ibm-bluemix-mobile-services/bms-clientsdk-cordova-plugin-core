3278f9235e97e1d3c5cd5c3faa8eaecdc8d3c0f8
vitalym <vitalym@il.ibm.com>
2015-11-09 12:54:33 +0200

    Implement registration of authentication listener

--- src/ios/CDVAuthorizationManager.swift
+++ src/ios/CDVAuthorizationManager.swift
@@ -11,16 +11,148 @@ import IMFCore
 
 @objc(CDVAuthorizationManager) class CDVAuthorizationManager : CDVPlugin {
     
+    enum CDVAuthManagerErrors : ErrorType {
+        case InvalidParameterCount
+        case InvalidParameterType
+    }
+    
     func obtainAuthorizationHeader(command: CDVInvokedUrlCommand) {
         let authManager = IMFAuthorizationManager.sharedInstance();
         
-        authManager.obtainAuthorizationHeaderWithCompletionHandler { (response: IMFResponse!, error: NSError!) -> Void in
-            var responseString: String?
+        self.commandDelegate!.runInBackground({
+        
+            authManager.obtainAuthorizationHeaderWithCompletionHandler { (response: IMFResponse!, error: NSError!) -> Void in
+                var responseString: String?
+                
+                do {
+                    if (error != nil) {
+                        // process the error
+                        try responseString = self.packResponse(response, error: error)
+                        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: responseString)
+                        self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                    } else {
+                        // process success
+                        try responseString = self.packResponse(response)
+                        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: responseString)
+                        self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                    }
+                
+                    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: responseString)
+                    self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                } catch {
+                    responseString = "Error Parsing JSON response."
+                    let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: responseString)
+                    self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                }
+            }
             
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: responseString)
-            self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        });
+        
+    }
+    
+    func isAuthorizationRequired(command: CDVInvokedUrlCommand) {
+        
+        self.commandDelegate!.runInBackground({
+            
+            do {
+                let authManager = IMFAuthorizationManager.sharedInstance()
+                let params = try self.unpackIsAuthorizationRequiredParams(command);
+                
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsBool:authManager.isAuthorizationRequired(params.statusCode, authorizationHeaderValue: params.authorizationHeaderValue))
+                
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            } catch {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "Invalid parameters passed to isAuthorizationRequired method")
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            }
+        })
+    }
+    
+    func clearAuthorizationData(command: CDVInvokedUrlCommand) {
+        self.commandDelegate!.runInBackground({
+            //let authManager = IMFAuthorizationManager.sharedInstance()
+            
+            //authManager.cl
+        })
+    }
+    
+    func getCachedAuthorizationHeader(command: CDVInvokedUrlCommand) {
+        self.commandDelegate!.runInBackground({
+            let authManager = IMFAuthorizationManager.sharedInstance()
+            
+            if let authHeader: String = authManager.cachedAuthorizationHeader {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString:authHeader)
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            } else {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "There is no cached authorization header.")
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            }
+        })
+    }
+    
+    func unpackIsAuthorizationRequiredParams(command: CDVInvokedUrlCommand) throws -> (statusCode: Int32, authorizationHeaderValue: String) {
+        if (command.arguments.count < 2) {
+            throw CDVAuthManagerErrors.InvalidParameterCount
         }
         
+        let param0 = command.argumentAtIndex(0)
+        let param1 = command.argumentAtIndex(1)
+        
+        if !(param0 is NSNumber) || !(param1 is NSString) {
+            throw CDVAuthManagerErrors.InvalidParameterType
+        }
+        
+        let statusValue: NSNumber = param0 as! NSNumber
+        let authHeader: String = String(param1)
+        
+        return (statusCode: statusValue.intValue, authorizationHeaderValue: authHeader)
+    }
+    
+    func packResponse(response: IMFResponse!,error:NSError?=nil) throws -> String {
+        let jsonResponse:NSMutableDictionary = [:]
+        var responseString: NSString = ""
+        
+        if error != nil {
+            jsonResponse.setObject(Int((error!.code)), forKey: "errorCode")
+            jsonResponse.setObject((error!.localizedDescription), forKey: "errorDescription")
+        }
+        else {
+            jsonResponse.setObject(Int((0)), forKey: "errorCode")
+            jsonResponse.setObject("", forKey: "errorDescription")
+        }
+        
+        if (response == nil)
+        {
+            jsonResponse.setObject("", forKey: "responseText")
+            jsonResponse.setObject([], forKey:"headers")
+            jsonResponse.setObject(Int(0), forKey:"status")
+        }
+        else {
+            let responseText: String = (response.responseText != nil)    ? response.responseText : ""
+            jsonResponse.setObject(responseText, forKey: "responseText")
+            
+            if response.responseHeaders != nil {
+                jsonResponse.setObject(response.responseHeaders, forKey:"headers")
+            }
+            else {
+                jsonResponse.setObject([], forKey:"headers")
+            }
+            
+            jsonResponse.setObject(Int(response.httpStatus), forKey:"status")
+            responseString = try self.stringifyResponse(jsonResponse);
+        }
+        return responseString as String
+    }
+    
+    func stringifyResponse(value: AnyObject,prettyPrinted:Bool = false) throws -> String {
+        let options = prettyPrinted ? NSJSONWritingOptions.PrettyPrinted : NSJSONWritingOptions(rawValue: 0)
+        var jsonString : String? = ""
+        
+        if NSJSONSerialization.isValidJSONObject(value) {
+            let data = try NSJSONSerialization.dataWithJSONObject(value, options: options)
+            jsonString = NSString(data: data, encoding: NSUTF8StringEncoding) as String?
+        }
+        return jsonString!
     }
     
 }
--- src/ios/CDVBMSClient.swift
+++ src/ios/CDVBMSClient.swift
@@ -10,6 +10,8 @@ import IMFCore
 
 @objc(CDVBMSClient) class CDVBMSClient : CDVPlugin {
     
+    static var jsChallengeHandlers:NSMutableDictionary = [:]
+    
     func initialize(command: CDVInvokedUrlCommand) {
 
         self.commandDelegate!.runInBackground({
@@ -71,5 +73,115 @@ import IMFCore
             self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
         })
         
+    }
+    
+    func registerAuthenticationListener(command: CDVInvokedUrlCommand) {
+        
+        func unpackParams() throws -> String {
+            return command.argumentAtIndex(0) as! String
+        }
+        
+        self.commandDelegate!.runInBackground({
+            do {
+                let realm = try unpackParams();
+                let client = IMFClient.sharedInstance()
+                let delegate = InternalAuthenticationDelegate(realm: realm, commandDelegate: self.commandDelegate!)
+                
+                client.registerAuthenticationDelegate(delegate, forRealm: realm)
+                
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+                self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+            } catch {
+                
+            }
+        })
+    }
+    
+    func unregisterAuthenticationListener(command: CDVInvokedUrlCommand) {
+        
+    }
+    
+    func addCallbackHandler(command: CDVInvokedUrlCommand) {
+        func unpackParams() throws -> String {
+            return command.argumentAtIndex(0) as! String
+        }
+        
+        do {
+            let realm = try unpackParams()
+            CDVBMSClient.jsChallengeHandlers.setValue(command, forKey: realm)
+        } catch {
+            
+        }
+        
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        pluginResult.setKeepCallbackAsBool(true)
+        self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+        
+    }
+    
+    internal class InternalAuthenticationDelegate : NSObject, IMFAuthenticationDelegate {
+        
+        var realm: String
+        var commandDelegate: CDVCommandDelegate
+        
+        init(realm: String, commandDelegate: CDVCommandDelegate) {
+            self.realm = realm
+            self.commandDelegate = commandDelegate
+        }
+        
+        /**
+         * Called when authentication challenge was received
+         @param context Authentication context
+         @param challenge Dictionary with challenge data
+         */
+        @objc @available(iOS 2.0, *)
+        internal func authenticationContext(context: IMFAuthenticationContext!, didReceiveAuthenticationChallenge challenge: [NSObject : AnyObject]!) {
+            
+            let command: CDVInvokedUrlCommand = jsChallengeHandlers[realm] as! CDVInvokedUrlCommand
+            
+            let jsonResponse: NSMutableDictionary = [:]
+            jsonResponse.setObject("action", forKey: "onAuthenticationChallengeReceived")
+            jsonResponse.setObject("challenge", forKey: challenge)
+            
+            let stringResult = self.stringifyResponse(jsonResponse)
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: stringResult)
+            pluginResult.setKeepCallbackAsBool(true)
+            commandDelegate.sendPluginResult(pluginResult, callbackId: command.callbackId)
+        }
+        
+        /**
+         * Called when authentication succeeded
+         @param context Authentication context
+         @param userInfo Dictionary with extended data about authentication success
+         */
+        @objc @available(iOS 2.0, *)
+        internal func authenticationContext(context: IMFAuthenticationContext!, didReceiveAuthenticationSuccess userInfo: [NSObject : AnyObject]!) {
+            
+        }
+        
+        /**
+         * Called when authentication failed.
+         @param context Authentication context
+         @param userInfo Dictionary with extended data about authentication failure
+         */
+        @objc @available(iOS 2.0, *)
+        internal func authenticationContext(context: IMFAuthenticationContext!, didReceiveAuthenticationFailure userInfo: [NSObject : AnyObject]!) {
+            
+        }
+        
+        func stringifyResponse(value: AnyObject,prettyPrinted:Bool = false) -> String {
+            let options = prettyPrinted ? NSJSONWritingOptions.PrettyPrinted : NSJSONWritingOptions(rawValue: 0)
+            var jsonString : String? = ""
+            
+            do {
+                if NSJSONSerialization.isValidJSONObject(value) {
+                    let data = try NSJSONSerialization.dataWithJSONObject(value, options: options)
+                    jsonString = NSString(data: data, encoding: NSUTF8StringEncoding) as String?
+                }
+            } catch {
+                
+            }
+            return jsonString!
+        }
     }
 }
