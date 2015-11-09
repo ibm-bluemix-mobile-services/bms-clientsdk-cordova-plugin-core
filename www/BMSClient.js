cd7a2bf736b009996710b0804d128f04ec72b5d8
rotembr <rotembr@il.ibm.com>
2015-11-09 11:32:14 +0200

    Remove cordova.define(..)
    Accidentally the file with cordova.define uploaded to git, this get
    generated automatically when the plugin installed.

--- www/BMSClient.js
+++ www/BMSClient.js
@@ -1,4 +1,3 @@
-cordova.define("ibm-mfp-core.BMSClient", function(require, exports, module) {
 /*
     Copyright 2015 IBM Corp.
     Licensed under the Apache License, Version 2.0 (the "License");
@@ -132,4 +131,3 @@ var BMSClient = function() {
 
 //Return singleton instance
 module.exports = new BMSClient();
-});
