ffeffaa48318daac5c1b4f0f13712e209dcf71d8
Matthew Parangot <mparang@us.ibm.com>
2016-05-31 11:31:49 -0500

    fixed travis

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Updating AndroidManifest.xml to minSdkVersion=15"
 cd platforms/android
-sed -i '' 's/android:minSdkVersion="14"/android:minSdkVersion="15"/' AndroidManifest.xml
+sed -i 's/minSdkVersion="14"/minSdkVersion="15"/' platforms/android/AndroidManifest.xml
 cd ../../
