8b17537d46792fa0becf92b973657aa046f48345
Parvathy Unnikrishnan <parvathy@Parvathys-MacBook-Pro.local>
2017-07-20 10:33:49 +0530

    Added new region constant for Germany

--- www/BMSClient.js
+++ www/BMSClient.js
@@ -29,6 +29,7 @@ var BMSClient = function() {
     this.REGION_US_SOUTH = ".ng.bluemix.net";
     this.REGION_UK = ".eu-gb.bluemix.net";
     this.REGION_SYDNEY = ".au-syd.bluemix.net";
+    this.REGION_GERMANY = ".eu-de.bluemix.net";
 
 
 
@@ -113,7 +114,7 @@ var BMSClient = function() {
     };
 
     /**
-     * 
+     *
      * @return backendGUID
      */
     this.getBluemixAppGUID = function(callback) {
@@ -135,4 +136,3 @@ var BMSClient = function() {
 
 //Return singleton instance
 module.exports = new BMSClient();
-
